a27d33c9a487fba2d6af9f684944136b95a765b5
Serhii Kononov <sergei97k@gmail.com>
2020-04-21 21:38:18 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const App = () => <div />;
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const About = () => {
+  return <Typography>About</Typography>;
+};
+
+export default About;
