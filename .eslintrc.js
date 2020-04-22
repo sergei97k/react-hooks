bb2085800934de464070f984eacd352516da8384
Serhii Kononov <sergei97k@gmail.com>
2020-04-22 21:07:50 +0300

    updated eslint extends and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  extends: ["airbnb", "plugin:react/recommended", "prettier", "prettier/react"],
+  extends: [
+    "react-app",
+    "airbnb",
+    "airbnb/hooks",
+    "prettier",
+    "prettier/react",
+  ],
   env: {
     browser: true,
     jest: true,
   },
   rules: {
-    "no-restricted-syntax": 0,
-    "import/no-unresolved": "off",
-    "react/prefer-stateless-function": "error",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "react/forbid-prop-types": 0,
   },
   parser: "babel-eslint",
   plugins: ["prettier"],
