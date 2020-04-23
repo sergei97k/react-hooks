14b6eb49c6f0a7c154985e35cac2f168b9010e29
Serhii Kononov <sergei97k@gmail.com>
2020-04-23 19:55:23 +0300

    added Container for App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { Header } from "./components";
 
-const App = () => (
-  <Router>
-    <Header />
+const useStyles = makeStyles(() => ({
+  appContainer: {
+    margin: "50px 0",
+  },
+}));
 
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
-  </Router>
-);
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <Header />
+
+      <Container maxWidth="xl" className={classes.appContainer}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
